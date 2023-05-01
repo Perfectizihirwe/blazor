1ec15b7d895a87bfeb948c7a84482cd0b26b316a
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-05-01 19:09:43 +0200

    Initial commit

---
+++ Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
