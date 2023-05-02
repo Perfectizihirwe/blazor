e13763fd92b76f0d025789d521adad22a44dedfd
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-05-02 14:25:01 +0200

    Todo list

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -1,9 +1,15 @@
-﻿namespace BlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
 {
     public class TodoItem
     {
+        [Required]
+        [MaxLength(50)]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
+        [MaxLength(50)]
         public string Description { get; set; } = string.Empty;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorApp.Data;
+using BlazorApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<Todos>();
-builder.Services.AddSingleton<TodoItem>();
+builder.Services.AddSingleton<TodoState>();
 
 
 var app = builder.Build();
